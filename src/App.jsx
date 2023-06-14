3511eb7a92d9ff7cda92dbef7b883328d56c5fbe
SantiagoLaraE <oscarescobar773@gmail.com>
2023-06-14 15:14:17 -0500

    Configuración react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
     <>
-      <Header/>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </MainLayout>
     </>
   );
 }
