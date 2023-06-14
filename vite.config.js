d76d92156dbbf19b98e245580b62006d3cc79d63
SantiagoLaraE <oscarescobar773@gmail.com>
2023-06-14 09:13:42 -0500

    Configuración alias Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@styles", replacement: path.resolve(__dirname, "src/styles") },
+      { find: "@components", replacement: path.resolve(__dirname, "src/components") },
+      { find: "@assets", replacement: path.resolve(__dirname, "src/assets") },
+    ],
+  },
+});
