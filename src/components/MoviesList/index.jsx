d8a39e52374fb92d0a50c064075ff10be57f38a8
SantiagoLaraE <oscarescobar773@gmail.com>
2023-06-15 15:25:28 -0500

    Header removido de MovieList

--- src/components/MoviesList/index.jsx
+++ src/components/MoviesList/index.jsx
@@ -1,26 +1,20 @@
 import React from "react";
-import Button from "@components/Button";
 import MovieCard from "@components/MovieCard";
 import "./MoviesList.scss";
 import { MovieCardLoading } from "../MovieCard";
 
-const MoviesList = ({ title, movies, scroll, loading }) => {
+const MoviesList = ({ movies, scroll, loading }) => {
   const loadingArr = ["", "", ""];
 
   return (
-    <section className="MoviesList trending-movies section">
-      <div className="container">
-        <div className="MoviesList__header">
-          <h2 className="MoviesList__title">{title}</h2>
-          <Button title="see all" size="small" />
-        </div>
-        <article className={`MoviesList__list${scroll ? "--scroll" : ""}`}>
-          {movies?.length && !loading && <Results movies={movies} />}
-          {!movies?.length && !loading && <NoResults />}
-          {loading && loadingArr.map((_, i) => <MovieCardLoading key={`MovieCardLoading-${i}`}/>)}
-        </article>
-      </div>
-    </section>
+    <article className={`MoviesList__list${scroll ? "--scroll" : ""}`}>
+      {movies?.length && !loading && <Results movies={movies} />}
+      {!movies?.length && !loading && <NoResults />}
+      {loading &&
+        loadingArr.map((_, i) => (
+          <MovieCardLoading key={`MovieCardLoading-${i}`} />
+        ))}
+    </article>
   );
 };
 
