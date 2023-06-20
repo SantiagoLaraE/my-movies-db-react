d349a8dfb92b95ef8a3bd6da43416414e9c39a98
SantiagoLaraE <oscarescobar773@gmail.com>
2023-06-20 11:51:06 -0500

    Función para convertir cadenas de texto en URLs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 /*
- * This function return an int number between two number
+ * This function returns an int number between two number
  * @params {min, max} min: minimun number to return, max: maximum number to return 
  * @return {number} Random number between given min & max numbers
  */
@@ -8,4 +8,15 @@ export function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+/*
+ * This function returns a formatted string in Kebab case 
+ * @params {string} any string to convert
+ * @return {string} formatted string in Kebab case. Just keep letters and numbers
+ */
+
+export function formatToURL(string) {
+  const regex = /\W+/gi
+  return string.toLowerCase().replaceAll(regex, '-');
 }
